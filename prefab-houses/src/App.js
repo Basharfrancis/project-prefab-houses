1c446468aab9426359034ec107cf61ede5b58597
Shirak <shirak.kevorkian@gmail.com>
2019-06-26 16:47:54 +0300

    cleanup

--- prefab-houses/src/App.js
+++ prefab-houses/src/App.js
@@ -1,28 +1,16 @@
 import React,{Component} from "react";
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import Autoplay from './component/Autoplay';
-import Contactform from "./component/Contactform";
-=======
-import Header from './Component/Header'
-import Autoplay from './Component/Autoplay'
->>>>>>> 82b6f89196af49a173ad54170ca62708d4f3a66b
+import Autoplay from './Component/Autoplay';
+import Contactform from "./Component/Contactform";
+import Header from "./Component/Header";
 
 class App extends Component{
     render(){
-<<<<<<< HEAD
     return(<div>
-
-        <Autoplay/>
-        <Contactform/>
-=======
-    return(
-        <div>
         <Header/>
         <Autoplay/>
-        
->>>>>>> 82b6f89196af49a173ad54170ca62708d4f3a66b
+        <Contactform/>
         </div>
     )
 }}
