82b6f89196af49a173ad54170ca62708d4f3a66b
Bashar <basharfrancis16@gmail.com>
2019-06-26 13:21:47 +0300

    add Header

--- prefab-houses/src/App.js
+++ prefab-houses/src/App.js
@@ -1,12 +1,17 @@
-import React  from "react";
+import React,{Component} from "react";
 import logo from './logo.svg';
 import './App.css';
-import Autoplay from './component/Autoplay'
+import Header from './Component/Header'
+import Autoplay from './Component/Autoplay'
 
-class App extends React.Component{
+class App extends Component{
     render(){
     return(
+        <div>
+        <Header/>
         <Autoplay/>
+        
+        </div>
     )
 }}
 export default App;
