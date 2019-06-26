fbcb9806587860196d6eb48306da6699d7b8685d
Shirak <shirak.kevorkian@gmail.com>
2019-06-26 16:10:03 +0300

    Contactform

--- prefab-houses/src/App.js
+++ prefab-houses/src/App.js
@@ -1,12 +1,16 @@
 import React  from "react";
 import logo from './logo.svg';
 import './App.css';
-import Autoplay from './component/Autoplay'
+import Autoplay from './component/Autoplay';
+import Contactform from "./component/Contactform";
 
 class App extends React.Component{
     render(){
-    return(
+    return(<div>
+
         <Autoplay/>
+        <Contactform/>
+        </div>
     )
 }}
 export default App;
