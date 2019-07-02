2e96b4055dbd34c6494a2944f9ab927a32d24599
Shirak <shirak.kevorkian@gmail.com>
2019-07-02 12:10:10 +0300

    footer

--- prefab-houses/src/App.js
+++ prefab-houses/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Autoplay from './Component/Autoplay';
 import Contactform from "./Component/Contactform";
 import Header from "./Component/Header";
+import Footer from "./Component/Footer";
 
 class App extends Component{
     render(){
@@ -11,6 +12,7 @@ class App extends Component{
         <Header/>
         <Autoplay/>
         <Contactform/>
+        <Footer/>
     </div>
        
         </div>
